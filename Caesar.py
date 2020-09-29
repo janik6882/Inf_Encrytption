7c41922e50c5d91443f96f3f5f3537439db01c25
Janik <janik@klauenberg.eu>
2020-09-29 14:13:03 +0200

    began with documentation

--- Caesar.py
+++ Caesar.py
@@ -19,8 +19,10 @@ normal_alphabet = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7,
                    "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14,
                    "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20,
                    "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26}
-crypt = {y:x for x,y in normal_alphabet.iteritems()}
+crypt = {y: x for x, y in normal_alphabet.iteritems()}
 allowed_chars = "abcdefghijklmnopqrstuvwxyz"
+
+
 def encrypt(inp, num):
     """
     Comment: encrypts a string using the caesar method.
@@ -30,28 +32,32 @@ def encrypt(inp, num):
             Dont even think of using an non Integer Value as num!!
             Only use a/A-z/Z for the input string.
     """
+    # checking input for validity
     try:
-        temp_var = int(num)
+        int(num)
     except ValueError:
         print ("RTFM. Please read the manual")
-        return
+        return None
     inp = inp.lower()
     if not check_string(inp, allowed_chars):
         print ("RTFM. Please read the manual")
-    pub = str()
-    temp = list()
+        return None
+    # validity checks ended, creating temporary vars for use in function
+    pub = str()  # pub is the resulting string which will be returned
+    temp = list()  # temporary List for storing the values of every char
     for i in inp:
         temp.append(normal_alphabet[i])
     for i in range(len(temp)):
         tempo = temp[i]+num
-        while tempo <=0:
-            tempo+=26
-        while tempo >26:
-            tempo-=26
-        temp[i]=tempo
+        while tempo <= 0:
+            tempo += 26
+        while tempo > 26:
+            tempo -= 26
+        temp[i] = tempo
     for i in temp:
-        pub+=crypt[i]
+        pub += crypt[i]
     return pub.upper()
+
 
 def check_string(to_test, allowed):
     """
@@ -60,11 +66,12 @@ def check_string(to_test, allowed):
     Output: Bool
     Special: Nothing special
     """
-
     if Set(to_test).issubset(Set(allowed)):
         return True
     else:
         return False
+
+
 def decrypt(inp, num):
     """
     Comment: decrypts a String(inp) using the caear algorithm
@@ -73,18 +80,18 @@ def decrypt(inp, num):
     Special: Uses encrypt() with negative integer for decryption. Further docu
             at encrypt()
     """
-
-    num-=2*num
+    num -= 2*num
     return encrypt(inp, num)
-
 
 
 if __name__ == '__main__':
     # Set Gui to True if you want to use the Gui, set to False of not.
     # Startvars:
     Gui = False
+
     def get_e1_val():
         Label(root, text=v1.get()).grid(row=3)
+
     def TK_decrypt():
         inp = v1.get()
         try:
@@ -99,6 +106,7 @@ if __name__ == '__main__':
             return
         decrypted = decrypt(inp, num)
         res.set(str(decrypted))
+
     def TK_encrypt():
         inp = v1.get()
         try:
@@ -114,16 +122,16 @@ if __name__ == '__main__':
         encrypted = encrypt(inp, num)
         res.set(str(encrypted))
 
-    #Wenn sie die Gui nicht nutzen wollen setzen sie bitte laufen zu true
     laufend = not Gui
     while laufend:
-        test1 = raw_input("wollen sie ver(1) oder entschluesseln(2) oder abbrechen(c):\n")
-        if test1=="1":
+        test1 = raw_input("""wollen sie ver(1) oder entschluesseln(2) oder
+                          abbrechen(c):\n""")
+        if test1 == "1":
             text1 = raw_input("ihr zu verschluesselnder text bitte:\n")
             num1 = int(raw_input("ihre verschiebung bitte:\n"))
             verschluesslt = "ihr verschluesselter text:\n{pub}"
             print verschluesslt.format(pub=encrypt(text1, num1))
-        elif test1=="2":
+        elif test1 == "2":
             text2 = raw_input("ihr zu entschluesselnder text bitte:\n")
             num2 = int(raw_input("ihre verschiebung bitte:\n"))
             entschluesselt = "ihr entschluesselter text:\n{priv}"
@@ -139,7 +147,7 @@ if __name__ == '__main__':
         Label(root, text="verschiebung").grid(row=1, column=0)
         e1 = Entry(root, textvariable=v1).grid(row=0, column=1)
         e2 = Entry(root, textvariable=v2).grid(row=1, column=1)
-        b1 = Button(text="encrypt", command=TK_encrypt).grid(row=2,column=0)
+        b1 = Button(text="encrypt", command=TK_encrypt).grid(row=2, column=0)
         b2 = Button(text="decrypt", command=TK_decrypt).grid(row=2, column=1)
         Label(root, text="Ergebnis: ").grid(row=3, column=0)
         x = Label(root, textvariable=res).grid(row=3, column=1)
