2d941b2d79b30e3cc222831973ce778349219850
Janik <janik@klauenberg.eu>
2020-10-02 20:16:52 +0200

    made caesar.py pthon3 compatible

--- Vignere.py
+++ Vignere.py
@@ -16,9 +16,22 @@ from sets import Set
 normal_alphabet = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7,
                    "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14,
                    "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20,
-                   "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26}
+                   "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26,
+                   " ": 27, "1": 28, "2": 29, "3": 30, "4": 31, "5": 32,
+                   "6": 33, "7": 34, "8": 35, "9": 36, "ä": 37, "ö": 38,
+                   "ü": 39,
+#                   "$": 39, "%": 40, "&": 41, "/": 42, "(": 43, ")": 44,
+#                   "=": 45, "?": 46, "´": 47, "`": 48, "{": 49, "[": 50,
+#                   "]": 51, "}": 52, "^": 53, "°": 54, "@": 55, "€": 56,
+#                   "*": 57, "+": 58, "~": 59, "'": 60, "#": 61, ">": 62,
+#                   "<": 63, "|": 64, ",": 65, ";": 66, ".": 67, ":": 68,
+#                   "-": 69, "_": 70,
+#                   "ö": 39, "ä": 40, "ü": 41
+                   }
 crypt = {y: x for x, y in normal_alphabet.iteritems()}
-allowed_chars = "abcdefghijklmnopqrstuvwxyz"
+allowed_chars = "".join(list(normal_alphabet.keys()))
+len_alpphabet = len(normal_alphabet)
+print len_alpphabet
 
 def encrypt(enc_str, key_str):
     enc_str = enc_str.lower()
@@ -36,19 +49,22 @@ def encrypt(enc_str, key_str):
         vals_key = vals_key * int(round((len(vals_enc)/float(len(vals_key)))+0.49))
     for i in range(len(enc_str)):
         vals_pub.append(int(vals_key[i]+vals_enc[i]))
+    print vals_pub
     for i in range(len(vals_pub)):
         val = vals_pub[i]
         while val <= 0:
-            val+=26
-        while val>26:
-            val-=26
-        pub+=crypt[val]
+            val += len_alpphabet
+        while val > len_alpphabet:
+            val -= len_alpphabet
+        print crypt[val]
+        pub += crypt[val]
     return pub
+
 
 def decrypt(dec_str, key_str):
     dec_str = dec_str.lower()
     key_str = key_str.lower()
-    pub= str()
+    pub = str()
     vals_pub = list()
     vals_dec = list()
     vals_key = list()
@@ -56,18 +72,19 @@ def decrypt(dec_str, key_str):
         vals_dec.append(normal_alphabet[i])
     for i in key_str:
         vals_key.append(normal_alphabet[i])
-    if len(vals_dec)>len(vals_key):
-        vals_key = vals_key * int(round((len(vals_dec)/float(len(vals_key)))+0.49))
+    if len(vals_dec) > len(vals_key):
+        vals_key = vals_key * int(round((len(vals_dec) / float(len(vals_key))) + 0.49))
     for i in range(len(vals_dec)):
         vals_pub.append(int(vals_dec[i]-vals_key[i]))
     for i in range(len(vals_pub)):
         val = vals_pub[i]
         while val <= 0:
-            val+=26
-        while val>26:
-            val-=26
-        pub+=crypt[val]
+            val += len_alpphabet
+        while val > len_alpphabet:
+            val -= len_alpphabet
+        pub += crypt[val]
     return pub
+
 
 def check_string(to_test, allowed):
     """
@@ -82,6 +99,6 @@ def check_string(to_test, allowed):
         return False
 
 
-print decrypt("abc", "ab")
-
-print encrypt("xyz", "ab")
+print (decrypt("xyz", "ab"))
+x = encrypt("xyz", "ab")
+print decrypt(x, "ab")
